a0e62c899ac7f350713c3d1822eb22afd259dfd3
Henry Lam <hcylam@connect.ust.hk>
2023-11-08 20:21:03 +0800

    adding the ability to add chinese and english names

--- server/routes/add_eaten_dish.py
+++ server/routes/add_eaten_dish.py
@@ -12,20 +12,21 @@ conn = connect.get_connection()
 
 @app.route("/add_eaten_dish", methods=['POST'])
 def add_eaten_dish():
-    # input in the format of {"search":"Swiss Chicken Wings"}
+    # input in the format of {"english":"Swiss Chicken Wings", "chinese": }
     if request.method == 'POST':
         cursor = conn.cursor()
-        user_input = request.json["search"]
+        english_input = request.json["english"]
+        chinese_input = request.json["chinese"]
         # handle if user input is english/chinese.
-        cursor.execute('SELECT * FROM foods WHERE EN_NAME = %s', [user_input])
+        cursor.execute('SELECT * FROM foods WHERE EN_NAME = %s OR CH_NAME = %s', [english_input, chinese_input])
         result = cursor.fetchall()
         curr_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         if not result:
             # searched name not found in db, make a new one.
-            cursor.execute('INSERT INTO foods (EN_NAME, MOST_RECENT_DATE) VALUES (%s, %s)', [user_input,curr_time])
+            cursor.execute('INSERT INTO foods (EN_NAME, CH_NAME, MOST_RECENT_DATE) VALUES (%s, %s, %s)', [english_input, chinese_input,curr_time])
             cursor.close()
-            return "Successfully added " + user_input + " into the database!"
+            return "Successfully added " + english_input + " and " + chinese_input + " into the database!"
         else:
             # add to history, just so user can do undos.
             cursor.execute('SELECT * FROM foods_history WHERE EN_NAME = %s ORDER BY MOST_RECENT_Date ASC', [user_input])
@@ -38,6 +39,6 @@ def add_eaten_dish():
             # searched name found in db, update the most recent eaten time.
             cursor.execute('UPDATE foods SET MOST_RECENT_DATE = (%s) WHERE id = %s', [curr_time, result[0][2]])
             cursor.close()
-            return "Updated the entry " + user_input
+            return "Updated the entry " + english_input + chinese_input
     else:
         return "Bad Request. Please use a POST request.", 400
